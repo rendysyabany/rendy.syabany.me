e77183290f03cdf974638088a9323460247a0ba9
Rendyansyah Syabany <rendy@syabany.com>
2023-08-11 22:47:33 +0700

    success get all post

--- app/writing/page.tsx
+++ app/writing/page.tsx
@@ -1,11 +1,46 @@
-import Content from "@/components/pages/writing/content";
+import React from 'react';
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
 
-export default async function Writing() {
-
-  return (
-    <>
-      <Content />
-    </>
-  );
+interface PostData {
+  data: {
+    title: string;
+    // Add more properties as needed
+  };
 }
 
+const getAllPostContents = (): PostData[] => {
+  const folder = "content/posts/";
+  const fileNames = fs.readdirSync(folder);
+  const postContents: PostData[] = [];
+
+  fileNames.forEach(fileName => {
+    if (fileName.endsWith('.md')) {
+      const filePath = path.join(folder, fileName);
+      const content = fs.readFileSync(filePath, 'utf8');
+      const matterResult = matter(content) as unknown as PostData;
+      postContents.push(matterResult);
+    }
+  });
+
+  return postContents;
+};
+
+const Writing = () => {
+  const post = getAllPostContents();
+
+  return (
+    <div>
+      <h1>All Posts</h1>
+      <ul>
+        <pre>{JSON.stringify(post.map(d => d.data), null, 1)}</pre>
+        {/* {post.content} */}
+        {/* {allPostContents.map((post, index) => (
+          <li key={index}>{post.data.title}</li>
+        ))} */}
+      </ul>
+    </div>
+  );
+};
+export default Writing;
