4f7808cba80332190268f69de7c2fe3f887f7ba8
Rendyansyah Syabany <rendy@syabany.me>
2024-10-01 13:02:22 +0700

    remove post api routes

--- app/api/posts/route.ts
+++
@@ -1,8 +0,0 @@
-// pages/api/posts.ts
-import type { NextApiRequest, NextApiResponse } from "next";
-import prisma from "@/lib/prisma";
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const posts = await prisma.post.findMany();
-  res.status(200).json(posts);
-}
